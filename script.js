a41938bb30bb6c276ba44037f0c8b09ea92a3877
MrBurp <andreass.ziegler@gmail.com>
2022-09-18 09:34:50 +0200

    conecting js

---
+++ script.js
@@ -0,0 +1,6 @@
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const lefBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+let activeSlide = 0;
